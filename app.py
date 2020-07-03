d994059cfd38e697ecc1249ed39c5ae5b0655383
Deepraj <bdeeprajrkm1@gmail.com>
2020-07-03 13:53:50 +0530

    added Tensorflow serving API to the Flask API

--- app.py
+++ app.py
@@ -1,58 +1,48 @@
-print("Initializing Tensorflow environment.....")
-import os
-#Remove Tensorflow Messages
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
-from tensorflow.python.util import deprecation
-#Avoid Deprecation warnings
-deprecation._PRINT_DEPRECATION_WARNINGS = False
-import tensorflow as tf
-from tensorflow import keras
 from flask import Flask,request,jsonify
 app = Flask(__name__) 
 from funcs import getClasses,bagOfWords,load_JSON,ProcessData
 from pathlib import Path
 import numpy as np
-# from model import get_prediction
-
-print("Loaded Tensorflow and other libraries.....")
+from model import get_prediction
+import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 root = Path(dir_path)
 
 json_path = root / "input" / "intents.json"
-saved_model_dir = "saved_model" 
-model_name = "TChatBot"
-model_version = "v1"
 
-model_path = saved_model_dir + "/" + model_name + "/" + model_version
+classes = getClasses()
+sorted_classes = sorted(classes)
 
-model = tf.keras.models.load_model(model_path)
+json_data = load_JSON(json_path)
+words,labels,training,output = ProcessData(json_data,train = False)
 
 @app.route('/api',methods=['POST'])
 def chat_output():
     data = request.get_json(force = True)
 
-    classes = getClasses()
-    sort_classes = sorted(classes)
-
-    json_data = load_JSON(json_path)
-    words,labels,training,output = ProcessData(json_data,train = False)
-
     #input text sent via POST request
     input_text = data["input"]
-    # model.summary()
     
-    tag_pred = model.predict([bagOfWords(input_text,words)])
+    #Prediction np array
+    tag_pred = get_prediction([bagOfWords(input_text,words).tolist()])
 
+    #Corresponding tag output
     chatOut_index = np.argmax(tag_pred)
-    tag = sort_classes[chatOut_index]
+
+    #Equivalent tag for the predicted index
+    tag = sorted_classes[chatOut_index]
+
+    #Getting the data for the corresponding tag in the JSON
     intents = json_data["intents"]
+
     reply = ""
     # confidence = chatOut[chatOut_index]
 
-    #Loop to randomly choose a reply
+    #Loop to randomly choose a reply for the corresonding tag
     for i in range(len(intents)):
         if intents[i]["tag"] == tag:
+            print("\nOutput tag: {}\n".format(tag))
             responses = intents[i]["responses"]
             reply = np.random.choice(responses, 1, replace=False)[0]  
             
--- model.py
+++ model.py
@@ -1,16 +1,20 @@
+from flask import jsonify
 import numpy as np
 import json
 import requests
 
-MODEL_URL='http://localhost:8501/v1/models/pets:predict'
+MODEL_URL='https://tchatbot1.herokuapp.com/v1/models/chat_model:predict'
 
 def get_prediction(bag_of_words):
 
     data = json.dumps({
-        'output': bag_of_words.tolist()
+    "signature_name": "serving_default",
+    "instances": bag_of_words
     })
+    headers = {"content-type": "application/json"}
 
-    response = requests.post(MODEL_URL, data=data.encode('utf-8'))
+    response = requests.post(url=MODEL_URL, data=data, headers=headers)
     result = json.loads(response.text)
-    return result
+    prediction = np.squeeze(result['predictions'][0])
+    return prediction
 
